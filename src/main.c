41e9f51fc0d29d653c5897d7ef3f9c68f31f48f2
Kaleb Szaro <kaleb.szaro@gmail.com>
2023-09-19 15:13:38 -0400

    testing main

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int main(int ac, char **argv){
+    char *prompt = "(Shell) $ ";
+    char *lineptr;
+    size_t n = 0;
+
+    (void)ac; (void)argv;
+
+    printf("%s", prompt);
+    getline(&lineptr, &n, stdin);
+    printf("%s\n", lineptr);
+
+    free(lineptr);
+
+    return 0;
+}
